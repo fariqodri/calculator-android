7e9b9218ece45428e0de442e7904707eee4528d3
fari.qodri <arfixfari@gmail.com>
2018-02-14 23:16:31 +0700

    Add signup pop up

--- app/src/main/java/com/kuwait/ristekcalculator/LoginActivity.java
+++ app/src/main/java/com/kuwait/ristekcalculator/LoginActivity.java
@@ -3,93 +3,85 @@ package com.kuwait.ristekcalculator;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
-import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Set;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_login );
-        final AutoCompleteTextView username = findViewById( R.id.usernameFill );
-        final Button signIn = findViewById( R.id.signIn );
-        Button signUp = findViewById( R.id.signUp );
-        final CheckBox show = findViewById( R.id.showPassword );
-        final ArrayList<String> usernames = new ArrayList<>(  );
-        final EditText password = findViewById( R.id.passwordFill );
-        final SharedPreferences data = this.getPreferences( Context.MODE_PRIVATE );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        final AutoCompleteTextView username = findViewById(R.id.usernameFill);
+        final Button signIn = findViewById(R.id.signIn);
+        TextView signUpRequest = findViewById(R.id.signUpRequest);
+        final CheckBox show = findViewById(R.id.showPassword);
+        final EditText password = findViewById(R.id.passwordFill);
+        final SharedPreferences data = this.getPreferences(Context.MODE_PRIVATE);
         Set<String> names = data.getAll().keySet();
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names.toArray(new String[names.size()]));
-        username.setAdapter( adapter );
-        show.setOnCheckedChangeListener( new CompoundButton.OnCheckedChangeListener() {
+        username.setAdapter(adapter);
+        show.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(!b) {
-                    password.setTransformationMethod( PasswordTransformationMethod.getInstance() );
-                }
-                else {
-                    password.setTransformationMethod( HideReturnsTransformationMethod.getInstance() );
-                }
-            }
-        } );
-        signUp.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final SharedPreferences.Editor write = data.edit();
-                String signedUpName = username.getText().toString();
-                String signedUpPassword = password.getText().toString();
-                if(signedUpName.equals("") || signedUpPassword.equals("")) {
-                    Toast.makeText(LoginActivity.this, "Your username or password can't be empty", Toast.LENGTH_LONG).show();
-                }
-                else if(data.getAll().containsKey(signedUpName)) {
-                    Toast.makeText(LoginActivity.this, "Sorry, username: " + signedUpName + " has been taken", Toast.LENGTH_LONG).show();
-                }
-                else{
-                    write.putString( signedUpName, signedUpPassword);
-                    write.commit();
-                    username.setText( "" );
-                    password.setText( "" );
-                    show.setChecked(false);
-                    Toast.makeText(LoginActivity.this, "You have been registered, please login with account" +
-                            " you just registered", Toast.LENGTH_LONG).show();
+                if (!b) {
+                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                } else {
+                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
                 }
             }
-        } );
-        signIn.setOnClickListener( new View.OnClickListener() {
+        });
+//
+        signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String uname = username.getText().toString();
-                String passwordData = data.getString( uname, null );
-                if(passwordData == null) {
+                String passwordData = data.getString(uname, null);
+                final SharedPreferences.Editor write = data.edit();
+                if (passwordData == null) {
                     Toast.makeText(LoginActivity.this, "There's no account with username: " + uname + ". Please sign up first", Toast.LENGTH_LONG).show();
-                }
-                else if(!passwordData.equals( password.getText().toString() )){
+                } else if (!passwordData.equals(password.getText().toString())) {
                     Toast.makeText(LoginActivity.this, "Password you just entered is wrong", Toast.LENGTH_LONG).show();
-                }
-                else {
-                    Intent i = new Intent( LoginActivity.this, CalculatorActivity.class );
-                    startActivity( i );
+                } else {
+                    Intent i = new Intent(LoginActivity.this, CalculatorActivity.class);
+                    startActivity(i);
                     finish();
                 }
             }
-        } );
+        });
+
+        password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_GO) {
+                    signIn.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        signUpRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SignUpFragment fragment = new SignUpFragment();
+                fragment.show(getSupportFragmentManager(), "SIGNUP");
+            }
+        });
+
     }
-
-
 }
---
+++ app/src/main/java/com/kuwait/ristekcalculator/SignUpFragment.java
@@ -0,0 +1,88 @@
+package com.kuwait.ristekcalculator;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by Fari on 2/14/2018.
+ */
+
+public class SignUpFragment extends AppCompatDialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        //Referensi: https://developer.android.com/guide/topics/ui/dialogs.html?hl=id
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        final View view = inflater.inflate(R.layout.activity_sign_up, null);
+        final CheckBox show = view.findViewById( R.id.showPassword );
+//      final ArrayList<String> usernames = new ArrayList<>(  );
+        final EditText password = view.findViewById( R.id.passwordFill );
+        final SharedPreferences data = getActivity().getPreferences( Context.MODE_PRIVATE );
+        final AutoCompleteTextView username = view.findViewById( R.id.usernameFill );
+
+        show.setOnCheckedChangeListener( new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(!b) {
+                    password.setTransformationMethod( PasswordTransformationMethod.getInstance() );
+                }
+                else {
+                    password.setTransformationMethod( HideReturnsTransformationMethod.getInstance() );
+                }
+            }
+        } );
+
+        builder.setView(view)
+                // Add action buttons
+                .setPositiveButton(R.string.registerButton, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // sign in the user ...
+                        final SharedPreferences.Editor write = data.edit();
+                        String signedUpName = username.getText().toString();
+                        String signedUpPassword = password.getText().toString();
+                        if(signedUpName.equals("") || signedUpPassword.equals("")) {
+                            Toast.makeText(getActivity(), "Your username or password can't be empty", Toast.LENGTH_LONG).show();
+                        }
+                        else if(data.getAll().containsKey(signedUpName)) {
+                            Toast.makeText(getActivity(), "Sorry, username: " + signedUpName + " has been taken", Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            write.putString( signedUpName, signedUpPassword);
+                            write.commit();
+                            username.setText( "" );
+                            password.setText( "" );
+                            show.setChecked(false);
+                            Toast.makeText(getActivity(), "You have been registered, please login with account" +
+                                    " you just registered", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        SignUpFragment.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
+
+
+}
